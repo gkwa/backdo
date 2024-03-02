aaf7c1cef9178c5857d92588195b789b73b54e2f
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-01 18:14:48 -0800

    stuff

---
+++ cmd/test1.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/taylormonacelli/backdo/test1"
+)
+
+var (
+	dirPath1        string
+	dirPath2        string
+	excludeExisting []string
+)
+
+// test1Cmd represents the test1 command
+var test1Cmd = &cobra.Command{
+	Use:   "test1",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if dirPath1 == "" || dirPath2 == "" {
+			return fmt.Errorf("both directory paths (--incoming and --existing) are required")
+		}
+		err := test1.RunTest(dirPath1, dirPath2, excludeExisting)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(test1Cmd)
+
+	test1Cmd.Flags().StringVar(&dirPath1, "incoming", "", "First directory path")
+	test1Cmd.Flags().StringVar(&dirPath2, "existing", "", "Second directory path")
+	test1Cmd.Flags().StringSliceVar(&excludeExisting, "exclude-existing", nil, "List of substrings to exclude from existing directory paths")
+}
---
+++ test1/test1.go
@@ -0,0 +1,109 @@
+package test1
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Match struct {
+	Path     string
+	RHS      []string
+	NotFound bool
+}
+
+func RunTest(dirPath1, dirPath2 string, excludeExisting []string) error {
+	// Generate lists of files from directory paths
+	list1 := generateFileList(dirPath1)
+	list2 := generateFileList(dirPath2)
+
+	// Apply substring filters to existing directory paths
+	list2 = applySubstringFilters(list2, excludeExisting)
+
+	// Create a map for faster lookup
+	list2Map := make(map[string]bool)
+	for _, path := range list2 {
+		list2Map[path] = true
+	}
+
+	// Create a list of matches
+	var matches []Match
+
+	// Loop over list 1
+	for _, path1 := range list1 {
+		baseName1 := filepath.Base(path1)
+		var rhs []string
+
+		// Loop over list 2 to find matches
+		for _, path2 := range list2 {
+			baseName2 := filepath.Base(path2)
+			if strings.EqualFold(baseName1, baseName2) {
+				rhs = append(rhs, path2)
+			}
+		}
+
+		// Append to matches if there are any matches
+		if len(rhs) > 0 {
+			matches = append(matches, Match{
+				Path: path1,
+				RHS:  rhs,
+			})
+		} else {
+			// If file not found in existing directory, mark it as not found
+			matches = append(matches, Match{
+				Path:     path1,
+				NotFound: true,
+			})
+		}
+	}
+
+	// Print matches
+	for _, match := range matches {
+		fmt.Printf("Path: %s\n", match.Path)
+		if match.NotFound {
+			fmt.Println("  Not found in existing directory")
+		} else {
+			for _, rhs := range match.RHS {
+				fmt.Printf("  RHS: %s\n", rhs)
+			}
+		}
+	}
+
+	return nil
+}
+
+func generateFileList(dirPath string) []string {
+	var fileList []string
+
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("Error walking directory: %v\n", err)
+	}
+
+	return fileList
+}
+
+func applySubstringFilters(paths []string, filters []string) []string {
+	filteredPaths := paths[:0] // Create a new slice with zero length
+
+Outer:
+	for _, path := range paths {
+		for _, filter := range filters {
+			if strings.Contains(path, filter) {
+				continue Outer // Skip this path if it contains any filter substring
+			}
+		}
+		filteredPaths = append(filteredPaths, path)
+	}
+
+	return filteredPaths
+}
